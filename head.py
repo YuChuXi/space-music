a802e56b7d7ef078eeedc72a8550442847b79013
YuChuXi <cyu245656@gmail.com>
2024-10-27 18:06:14 +0800

    sf

--- head.py
+++ head.py
@@ -1,24 +1,9 @@
 import librosa
-import numpy
+import soundfile
+import numpy as np
 
 
-head_zize = 0.35
 eps = 1e-5
-
-A = (1, 2)
-
-"""
-        ^
-        |
-        |  A
-        | /
-        |/
---------o-------->
-        |
-        |
-        |
-        |
-"""
 
 
 def distance(p0, p1):
@@ -36,14 +21,45 @@ def amp_weaken(dl, dr):
 def watt_weaken(dl, dr):
     return amp_weaken(dl, dr)**2
 
-def image(audio, p0, head_size):
+def image(audio, p0, head_size, r):
     dl = distance(p0, (0.5*head_size, 0)) + eps
     dr = distance(p0, (0.5*head_size, 0)) + eps
-    delay = sample_delay(delay(dl, dr))
+    delay = sample_delay(delay(dl, dr), r)
     weaken = amp_weaken(dl, dr)
+    if delay >0:
+        al = np.pad(audio, (delay, 0), mode="constant")[:delay]
+        ar = audio
+    elif delay < 0:
+        al = audio
+        ar = np.pad(audio, (-delay, 0), mode="constant")[:-delay]
+    else:
+        al = ar = audio
 
-    return audio
+    if weaken >1:
+        ar = ar / weaken
+    elif weaken < 1:
+        al = al * weaken
+    
+    return np.column_stack((al, ar))
 
 
-audio = librosa.load("0_1.wav")
-print(image(audio))
+
+A = (1, 2)
+"""
+        ^
+        |
+        |  A
+        | /
+        |/
+--------o-------->
+        |
+        |
+        |
+        |
+"""
+
+head_size = 0.35
+audio, r = librosa.load("0_1.wav")
+naudio = image(audio, A, head_size, r)
+print(naudio)
+soundfile.write("0_2.wav", naudio, r, )
