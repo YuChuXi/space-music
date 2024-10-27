c13ea60881644b194634d323a8c076cb1cf6ecf7
YuChuXi <cyu245656@gmail.com>
2024-10-27 17:33:11 +0800

    head

---
+++ head.py
@@ -0,0 +1,45 @@
+import librosa
+import numpy
+
+
+head_zize = 0.35
+eps = 1e-5
+
+A = (1, 2)
+
+"""
+        ^
+        |
+        |  A
+        | /
+        |/
+--------o-------->
+        |
+        |
+        |
+        |
+"""
+
+
+def distance(p0, p1):
+    return ((p0[0] - p1[0]) ** 2 + (p0[1] - p1[1]) ** 2) ** 0.5
+
+def delay(dl, dr):
+    return (dl - dr) / 340
+
+def sample_delay(t, r):
+    return r * t
+
+def amp_weaken(dl, dr):
+    return dl / dr
+
+def watt_weaken(dl, dr):
+    return amp_weaken(dl, dr)**2
+
+def image(p0, head_size):
+    dl = distance(p0, (0.5*head_size, 0)) + eps
+    dr = distance(p0, (0.5*head_size, 0)) + eps
+    return (sample_delay(delay(dl, dr)), amp_weaken(dl, dr))
+
+
+librosa.load()
